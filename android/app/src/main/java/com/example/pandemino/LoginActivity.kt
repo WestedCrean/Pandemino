2ca63c0afb9ea65a632c208fae085800a95f3a4d
Fileonii <yahhoofj@gmail.com>
2020-05-24 21:52:12 +0200

    24.05.2020 Login i Rejestracja

---
+++ android/app/src/main/java/com/example/pandemino/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.pandemino
+import android.R
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
--- android/app/src/main/java/com/example/pandemino/MainActivity.kt
+++ android/app/src/main/java/com/example/pandemino/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.pandemino
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+private val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        register_button_register.setOnClickListener {
+
+
+            val username = username_edittext_register.text.toString()
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d(TAG, "UTWORZONE KONTO: $username , $email, $password")
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d(TAG,"NOWY UZYTKOWNIK O UID ${it.result?.user?.uid}")
+                }
+        }
+
+
+
+
     }
 }
