96b51090b0bd9167cbb66eb64381c5d063e1683b
Fileonii <yahhoofj@gmail.com>
2020-05-25 21:33:14 +0200

    Fixed GitIgnore

--- android/app/src/main/java/com/example/pandemino/LoginActivity.kt
+++ android/app/src/main/java/com/example/pandemino/LoginActivity.kt
@@ -1,5 +1,5 @@
 package com.example.pandemino
-import android.R
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,12 +7,15 @@ import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_login.*
 
+//elementy layoutu nazywane są ze wzoru: corobi_czymjest_zjakiegowidoku
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_login)
+
 
     }
 }
--- android/app/src/main/java/com/example/pandemino/MainActivity.kt
+++ android/app/src/main/java/com/example/pandemino/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pandemino
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,17 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        ///WAŻNE
+        //elementy layoutu nazywane są ze wzoru: corobi_czymjest_zjakiegowidoku
         register_button_register.setOnClickListener {
 
-
+            //value które otrzymujemy z edittextów
             val username = username_edittext_register.text.toString()
             val email = email_edittext_register.text.toString()
             val password = password_edittext_register.text.toString()
 
             Log.d(TAG, "UTWORZONE KONTO: $username , $email, $password")
 
+            //Funkcja dodająca nowy rekord do firebase'a
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener {
                     if(!it.isSuccessful) return@addOnCompleteListener
@@ -33,6 +35,18 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG,"NOWY UZYTKOWNIK O UID ${it.result?.user?.uid}")
                 }
         }
+
+        login_textview_register.setOnClickListener {
+
+
+            Log.d(TAG,"Nowa Aktywność: LOGIN ACTIVITY")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
+
 
 
 
