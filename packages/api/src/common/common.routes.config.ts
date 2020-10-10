39288d5404e3cc2e0ecb957863f94a1316046a04
Wiktor Flis <wikflis@gmail.com>
2020-10-10 19:39:58 +0200

    Add backend structure

---
+++ packages/api/src/common/common.routes.config.ts
@@ -0,0 +1,17 @@
+import express from "express"
+
+export class CommonRoutesConfig {
+    app: express.Application
+
+    name: string
+    constructor(app: express.Application, name: string) {
+        this.app = app
+        this.name = name
+    }
+
+    getName() {
+        return this.name
+    }
+}
+
+export interface configureRoutes {}
