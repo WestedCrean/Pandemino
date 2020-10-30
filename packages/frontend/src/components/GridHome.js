b2ac1faf5d33c7b7090d394d84accd6c9f44613d
Fileonii <yahhoofj@gmail.com>
2020-10-30 20:04:26 +0100

    HomePage View fixed 1#

--- packages/frontend/src/components/GridHome.js
+++ packages/frontend/src/components/GridHome.js
@@ -1,23 +1,25 @@
 import React, { Fragment, useEffect, useState } from "react"
 import { Navbar, ListCourses } from "components"
 import AddCourseModal from "./AddCourseModal"
+
 const GridHome = () => {
     return (
         <div class="wrapper">
             <div class="box grid-courses">
+                <div className="box-label">
+                    <div className="box-label-name">TWOJE KURSY</div>
+                </div>
                 <ListCourses></ListCourses>
+                <div className="box-addNewCourse">
+                    <AddCourseModal></AddCourseModal>
+                </div>
             </div>
-            <div class="box grid-newCourse">
-                <AddCourseModal></AddCourseModal>
-            </div>
+
             <div class="box grid-news">
-                Aktualności
-                <br />
-                *Tutaj będą streamy dodane ostatnio z "naszych wykładów"*
-            </div>
-            <div class="box grid-incomingStreams">
-                Transmisje na żywo <br />
-                Tu zobaczymy co będzie
+                <div className="box-label">
+                    <div className="box-label-name">AKTUALNOŚCI</div>
+                </div>
+                TUTAJ SĄ NAJNOWSZE WYKŁADY
             </div>
         </div>
     )
