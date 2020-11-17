a3b585e66d5fbff1dd461a3a3f5c6c59d9330661
Wetido <wetido@gmail.com>
2020-11-18 00:27:38 +0100

    UserCourses fixed

--- packages/frontend/src/pages/ListCourses.js
+++ packages/frontend/src/pages/ListCourses.js
@@ -21,7 +21,7 @@ const ListCourses = () => {
     const userEmail = user.email
     const api = ApiService(accessToken)
 
-    const moveToCourse = (id) => {
+    const directToLecture = (id) => {
         history.push({
             pathname: `/course/${id}`,
             state: {
@@ -91,15 +91,23 @@ const ListCourses = () => {
             setUserCoursesId(listId)
         }
     }
+
+    
     const deleteUserCourses = async (id) => {
+       
         try {
-            await api.deleteUserCourse(id)
+            await api.deleteUserCourse(userCoursesId[id])
         } catch (error) {
             console.error(error)
         }
+
+        window.alert("Usunięto Cię z wykładu")
+        window.location = "/listCourses"
     }
 
+
     const joinCourse = async (courseId) => {
+        
         const body = {
             userId: userId,
             courseId: courseId,
@@ -108,18 +116,9 @@ const ListCourses = () => {
             .addUserCourse(body)
             .then((response) => console.log(response.data))
             .catch((error) => console.log(error))
-        window.alert("Dodano uzytkownika do wykladu wykladu")
-    }
-    const handleJoinCourse = async (course) => {
-        console.log({ course })
-        await joinCourse(course.id)
-        getUserCourses()
-    }
 
-    const handleQuitCourse = async (course) => {
-        console.log({ course })
-        await deleteUserCourses(course.id)
-        getUserCourses()
+        window.alert("Dodano Cię do wykladu")
+        window.location = "/listCourses"
     }
 
 
@@ -128,76 +127,82 @@ const ListCourses = () => {
     }
 
     return (
-        <div className="wrapper">
-            <div className="box grid-courses">
-                <div className="box-label">
-                    <div className="box-label-name">WSZYSTKIE KURSY</div>
-                </div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th>Nazwa </th>
-                            <th>Wykładowca</th>
-                            <th>Stream</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {courses.map((course, i = 0) => (
-                            <tr key={`${course.id}`}>
-                                <td>{i + 1}</td>
-                                <td>{course.name}</td>
-                                <td>{course.lecturer.email}</td>
-                                <td>
-                                    <Button
-                                        variant="dark"
-                                        onClick={() =>
-                                            moveToCourse(course.id)
-                                        }
-                                    >
-                                        Stream
-                                    </Button>
-                                </td>
-                                <td>
-                                    {existedCourseLists.includes(course.id) ===
-                                        false ? (
+        <div>
+            <Fragment>
+                <div class="wrapper">
+                    <div class="box grid-courses">
+                        <div className="box-label">
+                            <div className="box-label-name">
+                                WSZYSTKIE KURSY
+                            </div>
+                        </div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th>Nazwa </th>
+                                    <th>Wykładowca</th>
+                                    <th>Stream</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {courses.map((course, i = 0) => (
+                                    <tr key={`${course.id}`}>
+                                        <td>{i + 1}</td>
+                                        <td>{course.name}</td>
+                                        <td>{course.lecturer.email}</td>
+                                        <td>
                                             <Button
                                                 variant="dark"
                                                 onClick={() =>
-                                                    handleJoinCourse(course)
+                                                    directToLecture(course.id)
                                                 }
                                             >
-                                                Dolacz do kursu
+                                                Stream
                                             </Button>
-                                        ) : (
-                                            <Button
-                                                onClick={() => handleQuitCourse(course)}
-                                                variant="danger"
-                                            >
-                                                Odejdź z kursu
-                                            </Button>
-                                        )}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <div className="d-flexs p-2">
-                    <div className="md-form active-pink active-pink-2 mb-3 mt-0">
-                        <input
-                            className="form-control"
-                            type="text"
-                            placeholder="Search"
-                            aria-label="Search"
-                            value={query}
-                            onChange={(e) => setQuery(e.target.value)}
-                        ></input>
-                    </div>
-                    <div className="box-addNewCourse">
-                        <AddCourseModal />
+                                        </td>
+                                        <td>
+                                            {existedCourseLists.includes(
+                                                course.id
+                                            ) === false ? (
+                                                <Button
+                                                    variant="dark"
+                                                    onClick={() =>
+                                                        joinCourse(course.id)
+                                                    }
+                                                >
+                                                    Dolacz do kursu
+                                                </Button>
+                                            ) : (
+                                                <Button
+                                                    onClick={() =>
+                                                        deleteUserCourses(
+                                                            existedCourseLists.indexOf(
+                                                                course.id
+                                                            )
+                                                        )
+                                                    }
+                                                    variant="danger"
+                                                >
+                                                    Odejdź z kursu
+                                                </Button>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div className="d-flexs p-2">
+                            <div className="md-form active-pink active-pink-2 mb-3 mt-0">
+                                <input className="form-control" type="text" placeholder="Search" aria-label="Search" value={query} onChange={e => setQuery(e.target.value)}></input>
+                            </div>
+                            <div className="box-addNewCourse">
+                                <AddCourseModal></AddCourseModal>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fragment>
         </div>
     )
 }
