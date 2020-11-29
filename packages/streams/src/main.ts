1397053ca6ff9cecf903b0f95d78eee09e1ec2c6
Wiktor Flis <wikflis@gmail.com>
2020-11-29 18:50:52 +0100

    Set Trust Proxy headers in main

--- packages/streams/src/main.ts
+++ packages/streams/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { Logger } from "@nestjs/common"
+import { NestExpressApplication } from "@nestjs/platform-express"
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule)
+    app.set("trust proxy", 1)
     await app.listen(5050)
     Logger.log(`Application is running on: ${await app.getUrl()}`)
 }
