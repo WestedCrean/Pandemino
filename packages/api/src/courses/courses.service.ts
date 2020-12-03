dad1b4f0100d29562fef9f965826f84f14d8206f
Jakub Kaliszuk <33351633+Wetido@users.noreply.github.com>
2020-12-04 00:33:23 +0100

    Feature/password (#81)
    * Crypting password
    
    * Password modal
    
    * Fixing security
    
    * Fixing modal

--- packages/api/src/courses/courses.service.ts
+++ packages/api/src/courses/courses.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from "@nestjs/typeorm"
 import { User } from "../users/users.entity"
 import { Repository } from "typeorm"
 import { Course } from "./courses.entity"
+import * as bcrypt from 'bcryptjs';
+
 
 @Injectable()
 export class CoursesService {
@@ -17,6 +19,7 @@ export class CoursesService {
         const course = new Course()
         let user: User
 
+        
         try {
             user = await this.userRepository.findOne(createCourseSchema.userId)
         } catch (e) {
@@ -24,11 +27,17 @@ export class CoursesService {
         }
 
         try {
+
+            const hashedPassword = await bcrypt.hash(createCourseSchema.password, 10);
+
             course.name = createCourseSchema.name
             course.description = createCourseSchema.description
             course.lecturer = user
+            course.password = hashedPassword
             course.createdAt = new Date()
             await this.coursesRepository.save(course)
+
+            course.password = undefined
             return course
         } catch (e) {
             throw new Error(e)
@@ -50,8 +59,14 @@ export class CoursesService {
         if (updateCourseSchema.description !== null) {
             course.description = updateCourseSchema.description
         }
+        if (updateCourseSchema.password !== null) {
+            course.password = await bcrypt.hash(updateCourseSchema.password, 10);
+        }
+
 
         await this.coursesRepository.save(course)
+
+        
     }
 
     // FIXME: add pagination
---
+++ packages/frontend/src/components/PasswordModal.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react"
+import { Modal, Button } from "react-bootstrap"
+import { useAuthContext } from "services/auth"
+import ApiService from "services/api"
+import { Fab } from "@material-ui/core"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const PasswordModal = (props) => {
+    const [password, setPassword] = useState(null)
+
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
+
+
+    return (
+        <>
+            <Button variant="dark" onClick={handleShow}>
+                Dolacz do kursu
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Podaj haslo</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Hasło</Modal.Body>
+                <form>
+                    <input
+                        type="password"
+                        className="form-control form-input"
+                        id="name"
+                        placeholder="Podaj haslo"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    
+                </form>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Anuluj
+                    </Button>
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        onClick={() => {props.joinCourse(props.courseId, password)}}
+                    >
+                        Dodaj
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default PasswordModal
