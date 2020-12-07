4c3ab60281bbbcbee46a1e76926fecfa710262d7
Jakub Kaliszuk <33351633+Wetido@users.noreply.github.com>
2020-12-07 16:42:42 +0100

    Fancy wave (#94)

--- packages/frontend/src/pages/HomePage.js
+++ packages/frontend/src/pages/HomePage.js
@@ -1,9 +1,17 @@
 import React from "react"
 import GridHome from "../components/GridHome"
+import FancyWave from "../components/FancyWave"
+
 
 const HomePage = () => {
     return (
-        <GridHome></GridHome>
+        
+        <div className="main-wrapper">
+            <FancyWave></FancyWave>
+            <GridHome></GridHome>
+            
+        </div>
+        
     )
 }
 
