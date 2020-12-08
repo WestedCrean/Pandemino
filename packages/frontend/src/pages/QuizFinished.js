425b3245e7a07506516e0154a89f5c2291ca0366
Wetido <wetido@gmail.com>
2020-12-08 19:34:49 +0100

    Quiz timer fixes. Style quizFinished points calculatin

--- packages/frontend/src/pages/QuizFinished.js
+++ packages/frontend/src/pages/QuizFinished.js
@@ -4,7 +4,7 @@ import { useAuthContext } from "services/auth"
 import ApiService from "services/api"
 import { Modal, Button, Card } from "react-bootstrap"
 import { useUserInfo } from "hooks"
-
+import FancyWave from "components/FancyWave"
 
 const QuizFinished = (props) => {
     
@@ -12,6 +12,7 @@ const QuizFinished = (props) => {
 
     const { accessToken } = useAuthContext()
 
+    const [points, setPoints] = useState(0)
     const quizId = props.location.state.quizId
     const userId = props.location.state.userId
 
@@ -20,8 +21,16 @@ const QuizFinished = (props) => {
         try {
             const api = ApiService(accessToken)
             const response = await api.getUserUserAnswers(userId, quizId)
-            setUserAnswers(response.data[0].userAnswer)
-        
+            const data = response.data[0].userAnswer
+            console.log(data)
+            setUserAnswers(data)
+            
+            let tempPoints = 0;
+            for(let i = 0; i < data.length; i++){
+                tempPoints += parseFloat(data[i].points);
+            }
+            setPoints(tempPoints)
+
         } catch (error) {
             console.log(error)
         }
@@ -33,14 +42,17 @@ const QuizFinished = (props) => {
 
     return (
         <div>
+        <FancyWave></FancyWave>
+        <div className="quiz-finished-cointainer">
+          
             <div class="row justify-content-center mt-5 mb-2">
                 <h2>Quiz zakończony</h2>
             </div>
-
-                    <div>
-                        <table class="table">
+    
+                    <div className="quiz-table">
+                        <table className="table">
                             <thead>
-                                <tr>
+                                <tr className="quiz-tr-1">
                                     <th scope="col">#</th>
                                     <th scope="col">Pytanie</th>
                                     <th scope="col">Odpowiedź</th>
@@ -49,19 +61,27 @@ const QuizFinished = (props) => {
                             </thead>
                             <tbody>
                                 {userAnswers.map((answer, i) => (
-                                    <tr>
+                                    <tr className={`quiz-tr-${i%2}`}>
                                         <th scope="row">{i + 1}</th>
                                         <td>{answer.question.content}</td>
                                         <td>{answer.answer}</td>
-                                        <td>{answer.points}</td>
+                                        <td>{parseFloat(answer.points).toFixed(2)}</td>
+                                        
                                     </tr>
-                                    
                                 ))}
+                                <tr className={`quiz-tr-${(userAnswers.length + 1)%2}`}>
+                                    <td></td>
+                                    <td></td>
+                                    <th>Suma punktów</th>
+                                    <td>{parseFloat(points).toFixed(2)}</td>
+                                </tr>
+            
                             </tbody>
                         </table>
                     </div>
+               
         </div>
-
+        </div>
     )
 }
 
